76f09b38d8d3cb82fd89a94ebc81dd42cbce0aa8
MikeHuijgen <mikeh162001@hotmail.com>
2025-04-30 14:58:30 +0200

    Made the script work with perlin noise

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour
@@ -8,23 +6,45 @@ public class TerrainGenerator : MonoBehaviour
     [SerializeField] private int width;
     [SerializeField] private int height;
     [SerializeField] private GameObject grassPrefab;
+    [SerializeField] private GameObject waterPrefab;
 
     [SerializeField] private float perlinNoiseZoomScale;
 
     private List<GameObject> _blocks = new List<GameObject>();
+
+    private float xOffset;
+    private float yOffset;
 
     [ExecuteInEditMode]
     public void GenerateTerrain()
     {
         DestroyTerrain();
 
+        xOffset = Random.Range(0f, 999999f);
+        yOffset = Random.Range(0f, 999999f);
+
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
+                var xCoord = (float)x / width * perlinNoiseZoomScale + xOffset;
+                var yCoord = (float)y / height * perlinNoiseZoomScale + yOffset;
+
+                var sample = Mathf.PerlinNoise(xCoord, yCoord);
                 var newPos = new Vector3(x,0,y);
-                var newBlock = Instantiate(grassPrefab, newPos, Quaternion.identity);
+                GameObject newBlock = null;
+
+                if (sample > .4f)
+                {
+                    newBlock = Instantiate(grassPrefab, newPos, Quaternion.identity);
+                }
+                else
+                {
+                    newBlock = Instantiate(waterPrefab, newPos, Quaternion.identity);                   
+                }
+
                 _blocks.Add(newBlock);
+                newBlock.transform.parent = transform;
             }
         }
     }
